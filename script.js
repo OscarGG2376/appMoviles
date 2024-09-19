e7fc3ee5d126b7fee1d92f509dc63d6515660639
OscarGG2376 <garcia.garcia.oscar2376@gmail.com>
2024-09-18 20:54:46 -0600

    Clase 2-  18-09-2024 Oscar Garcia Garcia

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// este es un comentario
+
+/* 
+las variables guardan informacion
+*/
 console.log('Hola Mundo');
+alert("ola");
+
+const nombre = "Oscar";
+let edad = 14;
+var edad2 = 123;
+
+/*Tipos de datos:
+string
+number
+float
+bigInt
+boolean
+char
+NaN
+Infinity
+Hexadecimal 
+Objects
+
+*/
+let a = 5;
+let b = 6;
+let suma = a+b;
+let resta = a-b;
+
+
+let mensaje = "Hola mundo";
+let numero = 42;
+let Estudiante = true;
