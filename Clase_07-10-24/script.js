c69ac54a4bebe8445b51757c0eec029a78f30f21
OscarGG2376 <garcia.garcia.oscar2376@gmail.com>
2024-10-09 20:45:00 -0600

    COMENTARIO EN JS

--- Clase_07-10-24/script.js
+++ Clase_07-10-24/script.js
@@ -1,24 +1,23 @@
+const display = document.getElementById('display'); // Obtiene el elemento "display"
 
-const display = document.getElementById('display');
-function appendToDisplay (value) {
+function appendToDisplay(value) { // Agrega "value" al display
     document.getElementById('display').value += value;
 }
 
-    function clearDisplay() {
-    document.getElementById('display').value ='';
-    }
+function clearDisplay() { // Limpia el "display"
+    document.getElementById('display').value = '';
+}
 
-    function deleteLast() {
+function deleteLast() { // Elimina el último carácter del "display"
     let displayValue = document.getElementById('display').value;
     document.getElementById('display').value = displayValue.slice(0, -1);
-    }
-    function calculateResult() {
-         
-        try {
-    let result = eval(document.getElementById('display').value);
-    document.getElementById('display').value = result;
+}
+
+function calculateResult() { // Calcula la expresion en el "display"
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result; // Muestra el resultado
     } catch (error) {
-    alert('Expresión inválida');
-   
+        alert('Expresión inválida'); // Muestra si es que existe un error
     }
 }
