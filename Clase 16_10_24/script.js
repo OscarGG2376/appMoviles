3a45b8549e27d9ddc3934b9f991e37cdfeef66c2
OscarGG2376 <garcia.garcia.oscar2376@gmail.com>
2024-10-16 20:44:10 -0600

    Clase_16_10_24

---
+++ Clase 16_10_24/script.js
@@ -0,0 +1,9 @@
+const h1 = document.createElement('h1');
+
+h1.textContent="JAJAJADJAJAJAJAJ XD";
+
+document.body.appendChild(h1);
+
+h1.querySelector('body').appendChild(h1);
+
+console.log(h1,textContent);
